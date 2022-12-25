a5c0b99cd2878e9bcf9b0027b78b913703f55652
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-12-25 18:43:44 +0530

    Create Construct Binary Search Tree from Preorder Traversal.cpp

---
+++ Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,24 @@
+/*https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/description/*/
+
+class Solution {
+public:
+TreeNode* solve(vector<int>preorder,int mini,int maxi,int &i){
+    if(i>=preorder.size()){
+        return NULL;
+    }
+    if(preorder[i]<mini || preorder[i]>maxi){
+        return NULL;
+    }
+    TreeNode* root=new TreeNode(preorder[i]);
+    i++;
+    root->left=solve(preorder,mini,root->val,i);
+    root->right=solve(preorder,root->val,maxi,i);
+    return root;
+}
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int mini=INT_MIN;
+        int maxi=INT_MAX;
+        int i=0;
+        return solve(preorder,mini,maxi,i);
+    }
+};
