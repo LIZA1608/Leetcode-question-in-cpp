eae1d2c33c4e81c390dee350c30cd6062f1bd9ff
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-01-01 22:58:09 +0530

    Create Merge k Sorted Lists.cpp

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,52 @@
+/*https://leetcode.com/problems/merge-k-sorted-lists/description/*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ class compare{
+     public:
+     bool operator()(ListNode* a,ListNode* b){
+         return a->val>b->val;
+     }
+ };
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,compare>mini;
+        int k=lists.size();
+        if(k==0){
+            return NULL;
+        }
+        for(int i=0;i<k;i++){
+            if(lists[i]!=NULL){
+                mini.push(lists[i]);
+            }
+        }
+        ListNode* head=NULL;
+        ListNode* tail=NULL;
+        while(mini.size()>0){
+            ListNode* temp=mini.top();
+            mini.pop();
+            if(temp->next!=NULL){
+                mini.push(temp->next);
+            }
+            if(head==NULL){
+                head=temp;
+                tail=temp;
+            }
+            else{
+                tail->next=temp;
+                tail=temp;
+            }
+        }
+        return head;
+    }
+};
