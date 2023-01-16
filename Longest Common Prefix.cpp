a25ed334d1f9d32eef8d6cf1a20ebdd13421357e
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-01-16 19:36:24 +0530

    Create Longest Common Prefix.cpp

---
+++ Longest Common Prefix.cpp
@@ -0,0 +1,17 @@
+/*https://leetcode.com/problems/longest-common-prefix/description/*/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& s) {
+        int ans=s[0].length();
+        int n=s.size();
+        for(int i=1;i<n;i++){
+             int j=0;
+             while(j<s[i].size() && s[i][j]==s[0][j]){
+                 j++;
+             }
+             ans=min(ans,j);
+        }
+        return s[0].substr(0,ans);
+    }
+};
